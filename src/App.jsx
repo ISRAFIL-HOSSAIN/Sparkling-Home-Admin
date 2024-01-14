8c56b64d4f1b874837f892142f0e48f70778cb30
Israfil-Digi360 <mailto:israfil.hossain@digigate360.com>
2024-01-14 22:34:44 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import React, { lazy } from "react";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import AppOutlet from "./outlets/AppOutlet";
+import AuthorizedOutlet from "./outlets/AuthorizedOutlet";
+import UnauthorizedOutlet from "./outlets/UnauthorizedOutlet";
+
+
+const Users = lazy(() => import("./page/Users"));
+const Profile = lazy(() => import("./page/Profile"));
+const Dashboard = lazy(() => import("./page/Dashboard"));
+const Support = lazy(() => import("./page/Support"));
+
+const Signin = lazy(() => import("./page/Signin"));
+
+const Stores = lazy(() => import("./page/Stores"));
+const Earnings = lazy(() => import("./page/Earnings"));
+const ControlPanel = lazy(() => import("./page/ControlPanel"));
+const Transport = lazy(() => import("./page/Transport"));
+
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppOutlet />,
+      children: [
+        //auth
+        {
+          path: "login",
+          element: <UnauthorizedOutlet />,
+          children: [
+            {
+              path: "",
+              element: <Signin />,
+            },
+            {
+              path: "*",
+              element: <Navigate to={"/login"} />,
+            },
+          ],
+        },
+
+        // main layout
+        {
+          path: "",
+          element: <AuthorizedOutlet />,
+          children: [
+            {
+              path: "",
+              element: <Dashboard />,
+            },
+            {
+              path: "stores",
+              element: <Stores />,
+            },
+            {
+              path: "users",
+              element: <Users />,
+            },
+            {
+              path: "profile",
+              element: <Profile />,
+            },
+            {
+              path: "earnings",
+              element: <Earnings />,
+            },
+            {
+              path: "control-panel",
+              element: <ControlPanel />,
+            },
+            {
+              path: "support",
+              element: <Support />,
+            },
+            {
+              path: "transport",
+              element: <Transport />,
+            },
+            {
+              path: "*",
+              element: <Navigate to={"/"} />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/utils/CommonFunction.js
@@ -0,0 +1,32 @@
+import { useMediaQuery, useTheme } from "@mui/material";
+import moment from "moment";
+import { months } from "../constants/Data/constantsData";
+
+const getCurrentMonth = () => {
+  let currentDate = new Date();
+  let currentMonthIndex = currentDate.getMonth();
+  let currentMonthName = months[currentMonthIndex];
+
+  return currentMonthName.value;
+};
+
+const isLargeScreen = () => {
+  let theme = useTheme();
+  let isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  return isLargeScreen;
+};
+
+const formatDateString = (date) => {
+  return !!date ? moment(date).format("DD MMM, YYYY") : "";
+};
+
+export const convertToTitleCase = (str = "") => {
+  if (typeof str !== "string") return str;
+
+  return str
+    .toLowerCase()
+    .replace(/(?:^|_)([a-z])/g, (_, group) => ` ${group.toUpperCase()}`)
+    .replace(/_/g, " ");
+};
+
+export { formatDateString, getCurrentMonth, isLargeScreen };
